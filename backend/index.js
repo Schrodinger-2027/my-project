a96fc93550c6bce241541494948825e1cf998924
Schrodinger-2027 <Schrodinger2027@gmail.com>
2026-03-23 23:37:26 +0530

    chat enabled

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,32 @@
 const express = require('express')
+const dotenv = require('dotenv')
+const {WebSocketServer} = require('ws')
+
 
 const app = express()
-const PORT = 9000
+const PORT = process.env.PORT | 9000
+
+
+const wss = new WebSocketServer({port : PORT})
+
+wss.on('connection' , (ws)=>{
+    ws.on('message' , (data)=>{
+        const message = data.toString();
+        
+        wss.clients.forEach((client)=>{
+            if(client != ws){
+                client.send('message is : ' + message)
+            }
+        })
+    })
+})
+
+app.get('/' , (req , res)=>{
+    res.send('Hello')
+})
+
+
 
 app.listen(PORT , ()=>{
-    console.log(`server is connected to ${PORT}`)
+    console.log(`server is connected to : ${PORT}`)
 })
